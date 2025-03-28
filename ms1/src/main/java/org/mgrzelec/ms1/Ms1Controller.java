25fe96a022ad7c48534664822fdc963ad4fbfe9d
Maciej Grzelec <M.Grzelec@wb.com.pl>
2025-03-28 13:32:25 +0100

    Dynamic change in config
    Change-Id: I6e6746608a53bcf2628de806b4813f1fe9de490b

---
+++ ms1/src/main/java/org/mgrzelec/ms1/Ms1Controller.java
@@ -0,0 +1,20 @@
+package org.mgrzelec.ms1;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+//dynamiczna zmiana konfiguracji - oprócz tego w application properties potrzebny jest refresh
+@RefreshScope
+@RestController
+public class Ms1Controller {
+
+	@Value("${message}")
+	private String message;
+
+	@GetMapping
+	public String hello() {
+		return message;
+	}
+}
